f6b666ca23c22107dc1bc0d8c794b903cfe1df97
laloupo <ayme93@hotmail.fr>
2016-05-16 16:49:07 +0200

    ajout des entity

---
+++ src/SamGunBundle/Entity/Candidature.php
@@ -0,0 +1,252 @@
+<?php
+
+namespace SamGunBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Candidature
+ *
+ * @ORM\Table(name="candidature")
+ * @ORM\Entity(repositoryClass="SamGunBundle\Repository\CandidatureRepository")
+ */
+class Candidature
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="prenom", type="string", length=255)
+     */
+    private $prenom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="mail", type="string", length=255)
+     */
+    private $mail;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="diplome", type="text")
+     */
+    private $diplome;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nomduposte", type="string", length=255)
+     */
+    private $nomduposte;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="motivation", type="text")
+     */
+    private $motivation;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="remarque", type="text", nullable=true)
+     */
+    private $remarque;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Candidature
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set prenom
+     *
+     * @param string $prenom
+     *
+     * @return Candidature
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set mail
+     *
+     * @param string $mail
+     *
+     * @return Candidature
+     */
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    /**
+     * Get mail
+     *
+     * @return string
+     */
+    public function getMail()
+    {
+        return $this->mail;
+    }
+
+    /**
+     * Set diplome
+     *
+     * @param string $diplome
+     *
+     * @return Candidature
+     */
+    public function setDiplome($diplome)
+    {
+        $this->diplome = $diplome;
+
+        return $this;
+    }
+
+    /**
+     * Get diplome
+     *
+     * @return string
+     */
+    public function getDiplome()
+    {
+        return $this->diplome;
+    }
+
+    /**
+     * Set nomduposte
+     *
+     * @param string $nomduposte
+     *
+     * @return Candidature
+     */
+    public function setNomduposte($nomduposte)
+    {
+        $this->nomduposte = $nomduposte;
+
+        return $this;
+    }
+
+    /**
+     * Get nomduposte
+     *
+     * @return string
+     */
+    public function getNomduposte()
+    {
+        return $this->nomduposte;
+    }
+
+    /**
+     * Set motivation
+     *
+     * @param string $motivation
+     *
+     * @return Candidature
+     */
+    public function setMotivation($motivation)
+    {
+        $this->motivation = $motivation;
+
+        return $this;
+    }
+
+    /**
+     * Get motivation
+     *
+     * @return string
+     */
+    public function getMotivation()
+    {
+        return $this->motivation;
+    }
+
+    /**
+     * Set remarque
+     *
+     * @param string $remarque
+     *
+     * @return Candidature
+     */
+    public function setRemarque($remarque)
+    {
+        $this->remarque = $remarque;
+
+        return $this;
+    }
+
+    /**
+     * Get remarque
+     *
+     * @return string
+     */
+    public function getRemarque()
+    {
+        return $this->remarque;
+    }
+}
+
---
+++ src/SamGunBundle/Entity/Poste.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace SamGunBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Poste
+ *
+ * @ORM\Table(name="poste")
+ * @ORM\Entity(repositoryClass="SamGunBundle\Repository\PosteRepository")
+ */
+class Poste
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="metier", type="text")
+     */
+    private $metier;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="gestionnaire", type="integer")
+     */
+    private $gestionnaire;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set metier
+     *
+     * @param string $metier
+     *
+     * @return Poste
+     */
+    public function setMetier($metier)
+    {
+        $this->metier = $metier;
+
+        return $this;
+    }
+
+    /**
+     * Get metier
+     *
+     * @return string
+     */
+    public function getMetier()
+    {
+        return $this->metier;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Poste
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set gestionnaire
+     *
+     * @param integer $gestionnaire
+     *
+     * @return Poste
+     */
+    public function setGestionnaire($gestionnaire)
+    {
+        $this->gestionnaire = $gestionnaire;
+
+        return $this;
+    }
+
+    /**
+     * Get gestionnaire
+     *
+     * @return int
+     */
+    public function getGestionnaire()
+    {
+        return $this->gestionnaire;
+    }
+}
+
